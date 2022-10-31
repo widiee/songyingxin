a35df7120c7bef43fe47a4fdcbddfaedde8a622b
IGER <IGER@SYX-TER>
2022-10-31 08:03:27 +0800

    commit

---
+++ src/xm.java
@@ -0,0 +1,23 @@
+
+public class xm {
+
+	public static void main(String[] args) {
+		  try {
+			  int a = 4,b =2,g=0;
+			  int f =a/g;
+		       System.out.println(f);
+			 int c = a/b;
+		        System.out.println(c);
+		        }catch(Exception e) {
+			  System.out.println(e.getMessage());
+		  
+		  }finally{
+			  System.out.println("异常结束");
+		  }
+		  System.out.println("程序结束");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+
